8977b10e425e0947dcbd23ce10cb0ff29ffc7a86
Tejumade Afonja <tmafonja1@gmail.com>
2024-03-13 17:11:15 +0100

    Revert "update .gitignore"
    This reverts commit 18011beaa155dca08589e2f522d1e2448d0cc05e.

--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .dataset import *
-from .ops import *
-from .misc import *
-from .logger import *
-from .eval import *
-from .csv import *
-from .rdp_accountant import *
-from progress.bar import Bar as Bar
