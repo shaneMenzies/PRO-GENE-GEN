7db7f2a3b46f0cb9ae569e98829f8cf8ad1bc216
Tejumade Afonja <tmafonja1@gmail.com>
2024-03-14 20:35:39 +0100

    add weight init

--- utils/ops.py
+++ utils/ops.py
@@ -1,6 +1,7 @@
 import torch
+from torch import nn
 
-__all__ = ["one_hot_embedding"]
+__all__ = ["one_hot_embedding", "weights_init"]
 
 
 def one_hot_embedding(y, num_classes=10, dtype=torch.FloatTensor, device="cpu"):
@@ -15,3 +16,8 @@ def one_hot_embedding(y, num_classes=10, dtype=torch.FloatTensor, device="cpu"):
     y_tensor = y.type(torch.LongTensor).view(*y.size(), -1).to(device)
     zeros = torch.zeros(*y.size(), num_classes).type(dtype).to(device)
     return zeros.scatter(scatter_dim, y_tensor, 1)
+
+def weights_init(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
